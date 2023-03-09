0cb3068a508b2c12ba8e8a2dcd7086dc7695476a
ymeriaf5 <younessmeriaf3@gmail.com>
2023-03-09 13:13:55 +0100

    first commit

--- src/main/java/com/example/azure_dev/AzureDevApplication.java
+++ src/main/java/com/example/azure_dev/AzureDevApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AzureDevApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AzureDevApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AzureDevApplication.class, args);
+    }
 
 }
